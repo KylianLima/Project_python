ffa9205ea727b5cf3e343b9175f2872b3fc37d0d
KylianLima <78018599+KylianLima@users.noreply.github.com>
2024-12-13 00:40:38 +0100

    Update case.py

--- case.py
+++ case.py
@@ -1,13 +1,50 @@
 from unite import*
 from jeu import*
+import pygame
+# Couleurs
+GRIS = (200, 200, 200)
+COULEUR_PORTEE = (100,100,100)
+NOIR = (0, 0, 0)
+BLEU = (0, 0, 255)  # Eau
+ROUGE = (255, 0, 0)  # Lave
+BLANC = (255, 255, 255)  # VIe
+
 
 class Case :
     def __init__(self,x,y,couleur): 
+        self.x = x
+        self.y = y
+        self.couleur = couleur
+    
+    def interact(self, unite):
         pass
     
-    def intercact():
-        pass
+    def dessiner_case(self, fenetre, taille):
+        pygame.draw.rect(fenetre, self.couleur, (self.x * taille, self.y * taille, taille, taille))
+
     
-    def dessiner_case():
+    
+class Lave(Case):
+    def __init__(self , x,y):
+        super().__init__(x, y, ROUGE)  
+    
+    def interact(self, unite):
+        print(f"{unite.nom} de l'{unite.equipe} est tuée par la lave !")
+        unite.points_de_vie = 0
+
+    
+class Eau(Case):
+    def __init__(self,x,y):
+        super().__init__(x, y, BLEU) 
+        
+    def interact(self, unite):
         pass
+
+class Vie(Case):
+    def __init__(self,x,y):
+        super().__init__(x, y, BLANC) 
+        
+    def interact(self, unite):
+        print(f"{unite.nom} de l'{unite.equipe} regagne 50 PV !")
+        unite.points_de_vie += 50
 
