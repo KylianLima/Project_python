92d7d913681d12971d9617465abe4fc3e7e39f9c
KylianLima <78018599+KylianLima@users.noreply.github.com>
2024-12-12 18:55:03 +0100

    Ajout class case

---
+++ case.py
@@ -0,0 +1,13 @@
+from unite import*
+from jeu import*
+
+class Case :
+    def __init__(self,x,y,couleur): 
+        pass
+    
+    def intercact():
+        pass
+    
+    def dessiner_case():
+        pass
+
