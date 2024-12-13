a84f42b50f91e70cf7ec050890e2a162591fdf82
KylianLima <78018599+KylianLima@users.noreply.github.com>
2024-12-13 21:14:49 +0100

    Update case.py

--- case.py
+++ case.py
@@ -1,6 +1,7 @@
 from unite import*
 from jeu import*
 import pygame
+
 # Couleurs
 GRIS = (200, 200, 200)
 COULEUR_PORTEE = (100,100,100)
@@ -16,7 +17,7 @@ class Case :
         self.y = y
         self.couleur = couleur
     
-    def interact(self, unite):
+    def interact(self, unite, cases_speciales):
         pass
     
     def dessiner_case(self, fenetre, taille):
@@ -28,23 +29,31 @@ class Lave(Case):
     def __init__(self , x,y):
         super().__init__(x, y, ROUGE)  
     
-    def interact(self, unite):
-        print(f"{unite.nom} de l'{unite.equipe} est tuée par la lave !")
-        unite.points_de_vie = 0
+    def interact(self, unite, cases_speciales):
+        
+        if not hasattr(unite, "survit_lave"):
+            unite.points_de_vie = 0
+            print(f"{unite.nom} de l'{unite.equipe} est tuée par la lave !")
 
     
 class Eau(Case):
     def __init__(self,x,y):
         super().__init__(x, y, BLEU) 
         
-    def interact(self, unite):
-        pass
+    def interact(self, unite, cases_speciales):
+        if not hasattr(unite, "nage") or not unite.nage():
+            print(f"{unite.nom} de l'{unite.equipe} ne peut pas traverser l'eau.")
+            return False  # Bloque le déplacement
+        else : 
+            return True
 
 class Vie(Case):
     def __init__(self,x,y):
         super().__init__(x, y, BLANC) 
         
-    def interact(self, unite):
+    def interact(self, unite, cases_speciales):
         print(f"{unite.nom} de l'{unite.equipe} regagne 50 PV !")
         unite.points_de_vie += 50
+        cases_speciales.remove(self)
+        return True
 
